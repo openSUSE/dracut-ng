153f2fb743c8ab2dc4adc9503d0b053794ce83de
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2025-11-05 13:31:42 +0100

    fix(dracut.sh): improve detection of installed kernel versions
    SUSE-specific patch needed to improve the detection of kernel versions installed
    on the system when running dracut with the `--regenerate-all` option.

--- dracut.sh
+++ dracut.sh
@@ -1084,6 +1084,7 @@ if [[ $regenerate_all == "yes" ]]; then
     if [[ $parallel != "yes" ]]; then
         for i in *; do
             [[ -f $i/modules.dep ]] || [[ -f $i/modules.dep.bin ]] || continue
+            [[ -d $i/kernel ]] || continue
             "$dracut_cmd" --kver="$i" "${dracut_args[@]}"
             _rc=$?
             if [[ $_rc -gt 0 ]]; then
@@ -1094,6 +1095,7 @@ if [[ $regenerate_all == "yes" ]]; then
     else
         for i in *; do
             [[ -f $i/modules.dep ]] || [[ -f $i/modules.dep.bin ]] || continue
+            [[ -d $i/kernel ]] || continue
             "$dracut_cmd" --kver="$i" "${dracut_args[@]}" &
         done
         while :; do
