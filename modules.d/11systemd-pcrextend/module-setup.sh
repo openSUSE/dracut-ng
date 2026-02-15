27ea7e5b48290c66a5d88dea6335dc6000dcf230
Jo Zzsi <jozzsicsataban@gmail.com>
2026-02-15 18:06:43 -0500

    fix(systemd-pcrextend): add NVPCR definition files
    According to https://github.com/systemd/systemd/issues/40159#issuecomment-3760597003
    the initramfs is expected to include the nvpcr definition files from systemd for
    v259 compatibility.
    
    (cherry picked from commit e6b27fd2bff886720a1e6295766ee22d07f37d9a)

--- modules.d/11systemd-pcrextend/module-setup.sh
+++ modules.d/11systemd-pcrextend/module-setup.sh
@@ -37,7 +37,8 @@ install() {
         "$systemdutildir"/systemd-pcrextend \
         "$systemdsystemunitdir"/systemd-pcrphase-initrd.service \
         "$systemdsystemunitdir/systemd-pcrphase-initrd.service.d/*.conf" \
-        "$systemdsystemunitdir"/initrd.target.wants/systemd-pcrphase-initrd.service
+        "$systemdsystemunitdir"/initrd.target.wants/systemd-pcrphase-initrd.service \
+        "/usr/lib/nvpcr/*.nvpcr"
 
     # Install the hosts local user configurations if enabled.
     if [[ $hostonly ]]; then
